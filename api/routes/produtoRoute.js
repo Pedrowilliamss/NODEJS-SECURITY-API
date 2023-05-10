aea58e51f5962949481a10248d9a1068ea9ca6c2
Pedro Williams <pedrowilliamsbc@gmail.com>
2023-05-10 10:58:04 -0300

    fix

--- api/routes/produtoRoute.js
+++ api/routes/produtoRoute.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 const ProdutoController = require('../controllers/produtoController.js')
 const roles = require('../middleware/roles.js')
+const permissoes = require('../middleware/permissoes.js')
 
 const router = Router()
 
 router
   .post('/produto', ProdutoController.cadastrarProduto)
-  .get('/produto', roles(['Gerente']) ,ProdutoController.buscarTodosProdutos)
+  .get('/produto', permissoes(['editar']) ,ProdutoController.buscarTodosProdutos)
   .get('/produto/id/:id', ProdutoController.buscarProdutoPorId)
   .delete('/produto/id/:id', ProdutoController.deletarProdutoPorId)
   .put('/produto/id/:id', ProdutoController.editarProduto)
